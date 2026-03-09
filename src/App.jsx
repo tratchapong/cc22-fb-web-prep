be857ad0bf31603010b0eb5e22cd42378836905f
Ratchapong <ratchapong@Ratchapongs-Mac-mini.local>
2026-03-10 00:09:14 +0700

    start with daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+
+  return (
+    <>
+      Fakebook App
+      <div className="flex gap-2 mt-4">
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-error">Error</button>
+      </div>
+    </>
+  )
+}
+
+export default App
