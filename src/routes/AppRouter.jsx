9629ab56f5c223fa476375adc25e6f5c16d58b33
Ratchapong <ratchapong@Ratchapongs-Mac-mini.local>
2026-03-10 12:08:00 +0700

    Lazy + Suspense in AppRouter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,8 +1,13 @@
-import Friends from '@/pages/Friends'
-import Home from '@/pages/Home'
-import Login from '@/pages/Login'
-import Profile from '@/pages/Profile'
 import {createBrowserRouter, Navigate, Outlet, RouterProvider} from 'react-router'
+// import Friends from '@/pages/Friends'
+// import Home from '@/pages/Home'
+// import Login from '@/pages/Login'
+// import Profile from '@/pages/Profile'
+import { lazy, Suspense } from 'react'
+const Login = lazy(()=>import('../pages/Login'))
+const Home = lazy(()=>import('../pages/Home'))
+const Friends = lazy(()=>import('../pages/Friends'))
+const Profile = lazy(()=>import('../pages/Profile'))
 
 const guestRouter = createBrowserRouter([
  {path : '/', element: <Login />},
@@ -30,7 +35,9 @@ function AppRouter() {
 //  const user = null
  const finalRouter = user ? userRouter : guestRouter
  return (
+  <Suspense fallback={<span className="loading loading-bars loading-md"></span>}>
    <RouterProvider router={finalRouter} />
+  </Suspense>
  )
 }
 
