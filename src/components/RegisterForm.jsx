4344477128aa9f1f8f1e75af3a21d8d5124a4b2b
tratchapong <tratchapong@gmail.com>
2026-03-10 23:20:28 +0700

    react-hook-form on Register.jsx

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,30 +1,57 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { registerSchema } from '@/validations/schema'
+
 
 function RegisterForm() {
+  const { formState, register, handleSubmit, reset } = useForm({
+    resolver: zodResolver(registerSchema),
+    mode: 'onSubmit',
+    defaultValues: { firstName: "", lastName: "", identity: "", password: "", confirmPassword: "" },
+  })
+  const { errors } = formState
+
+  const onSubmit = async data => {
+    try {
+      alert(JSON.stringify(data, null, 2))
+      // const resp = await axios.post('http://localhost:8899/api/auth/register', data)
+      // console.log(resp)
+    } catch (err) {
+      console.log(err)
+    }
+  }
   return (
     <>
       <div className="text-3xl text-center opacity-70">Create a new account</div>
       <div className="divider opacity-60"></div>
-      <form onSubmit={e => e.preventDefault()} className='flex flex-col gap-5 p-4 pt-3'>
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-5 p-4 pt-3'>
         <div className="flex gap-2">
           <input type="text"
+            {...register('firstName')}
             placeholder='First name'
             className='input input-bordered w-full' />
           <input type="text"
+            {...register('lastName')}
             placeholder='Last name'
             className='input input-bordered w-full' />
         </div>
         <input type="text"
-         placeholder='Email or Phone number'
-         className='input input-bordered w-full' />
-       <input type="password"
-         placeholder='New password'
-         className='input input-bordered w-full' />
-       <input type="password"
-         placeholder='Confirm password'
-         className='input input-bordered w-full' />
-       <button className='btn btn-secondary text-xl text-white'>Sign up</button>
+          {...register('identity')}
+          placeholder='Email or Phone number'
+          className='input input-bordered w-full' />
+        <input type="password"
+          {...register('password')}
+          placeholder='New password'
+          className='input input-bordered w-full' />
+        <input type="password"
+          {...register('confirmPassword')}
+          placeholder='Confirm password'
+          className='input input-bordered w-full' />
+        <button className='btn btn-secondary text-xl text-white'>Sign up</button>
       </form>
+      <div className="border">
+        <pre className="text-error text-xs">{JSON.stringify(errors, (k,v) => k==='ref' ? undefined : v, 2) }</pre>
+      </div>
     </>
   )
 }
